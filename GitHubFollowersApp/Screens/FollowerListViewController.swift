0de5c7cb6a26dbd782e676d961965f139dc00b74
berrrchik <berrrchik@mail.ru>
2026-02-10 20:53:45 +0300

    feat: implement network manager into follower list vc

--- GitHubFollowersApp/Screens/FollowerListViewController.swift
+++ GitHubFollowersApp/Screens/FollowerListViewController.swift
@@ -7,7 +7,19 @@ class FollowerListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFolllowers(for: username, page: 1) { (followers, errorMessage) in
+            guard let followers = followers else {
+                self.presentGFAllertOnMainThread(title: "Bad stuff happend", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+
     }
 }
