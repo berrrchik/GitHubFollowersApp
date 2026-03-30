6895ae22665ce2a30b8661d4ba00b77929acb078
berrrchik <berrrchik@mail.ru>
2026-03-30 20:57:35 +0300

    feat: add loading view to follower list view controller

--- GitHubFollowersApp/Screens/FollowerListViewController.swift
+++ GitHubFollowersApp/Screens/FollowerListViewController.swift
@@ -39,8 +39,10 @@ class FollowerListViewController: UIViewController {
     }
     
     func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFolllowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
             switch result {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
