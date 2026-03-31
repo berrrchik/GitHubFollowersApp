8aeb9e80df16433ae24748c40fede7277e65beb1
berrrchik <berrrchik@mail.ru>
2026-03-31 19:40:28 +0300

    feat: add header view to user info view controller

--- GitHubFollowersApp/Screens/UserInfoViewController.swift
+++ GitHubFollowersApp/Screens/UserInfoViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class UserInfoViewController: UIViewController {
     
+    let headerView = UIView()
+    
     var username: String!
     
     override func viewDidLoad() {
@@ -10,15 +12,38 @@ class UserInfoViewController: UIViewController {
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissViewController))
         navigationItem.rightBarButtonItem = doneButton
         
+        layoutUI()
+        
         NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
             guard let self = self else { return }
             switch result {
             case .success(let user):
-                print(user)
+                DispatchQueue.main.async {
+                    self.add(childViewController: GFUserInfoHeaderViewController(user: user), to: self.headerView)
+                }
             case .failure(let error):
                 self.presentGFAllertOnMainThread(title: "Что-то пошло не так", message: error.rawValue, buttonTitle: "Ок")
             }
         }
+    }
+    
+    func layoutUI() {
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+                
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+    }
+    
+    func add(childViewController: UIViewController, to containerView: UIView) {
+        addChild(childViewController)
+        containerView.addSubview(childViewController.view)
+        childViewController.view.frame = containerView.bounds
+        childViewController.didMove(toParent: self)
     }
     
     @objc func dismissViewController() {
